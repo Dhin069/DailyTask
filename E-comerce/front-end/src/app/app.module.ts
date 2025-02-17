4107dd1abfe276fab463c12b702fc790e7c8d4b8
Dhin3 <gvthin3sh@gmail.com>
2025-02-18 00:04:18 +0530

    form handeller fn initialized

--- E-comerce/front-end/src/app/app.module.ts
+++ E-comerce/front-end/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ProductComponent } from './product/product.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- E-comerce/front-end/src/app/user/user.component.ts
+++ E-comerce/front-end/src/app/user/user.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
 
+  @ViewChild('userForm') form = NgForm;
+  id: string =''
+  name: string=''
+  email: string=''
+
+  onGetByID(){
+
+    console.log("onGetByID", this.id);
+    
+  }
+  onDelByID(){
+    console.log("OonDelByID", this.id);
+    
+  }
+  onGetAll(){
+    console.log("onGetAll");
+    
+  }
+  onInsert(){
+    console.log("onInsert", this.id, this.name, this.email);
+    
+  }
+  onUpdate(){
+    console.log("onUpdate", this.id, this.name, this.email);
+    
+  }
 }
