6e4b8fdd4fd2e3ec85de89d555e1f8a64a66d931
Dhinesh V. <VDhinesh@CEIIND.COM>
2025-02-17 19:20:09 +0530

    user styling added

--- E-comerce/front-end/src/app/app.module.ts
+++ E-comerce/front-end/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { ProductComponent } from './product/product.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
     ProductComponent,
-    HeroSectionComponent
+    HeroSectionComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
